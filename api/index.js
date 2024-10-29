8cc751df7d65cf3f15c58d2c5476b8b712147f80
Yousaf Raza <yousafrazaravian@gmail.com>
2024-10-29 06:27:58 +0500

    packages updated node-fetch removed axios used as alternative
    Signed-off-by: Yousaf Raza <yousafrazaravian@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import Fastify from 'fastify';
-import fetch from "node-fetch";
+import axios from 'axios';
 
 const app = Fastify({
   logger: true,
@@ -9,28 +9,29 @@ app.get('/healthCheck', async (req, reply) => {
   return reply.status(200).send({ message: "ALL OK!"})
 })
 app.get('/accessToken', async (req, reply) => {
-
-
   try {
     const code = req.query.code; // Capture the `code` from query parameters
 
     if (!code) {
       return reply.status(400).send('Missing "code" query parameter');
     }
-    const response = await fetch('https://github.com/login/oauth/access_token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json', // GitHub expects this header to return JSON
-      },
-      body: JSON.stringify({
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        code: code,
-      }),
-    });
 
-    const data = await response.json();
+    const response = await axios.post(
+        'https://github.com/login/oauth/access_token',
+        {
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          code: code,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json', // GitHub expects this header to return JSON
+          },
+        }
+    );
+
+    const data = response.data;
 
     if (data.error) {
       return reply.status(400).send({ error: data.error_description });
@@ -44,7 +45,6 @@ app.get('/accessToken', async (req, reply) => {
     return reply.status(500).send({ error: 'Internal Server Error' });
   }
 });
-
 export default async function handler(req, reply) {
   await app.ready()
   app.server.emit('request', req, reply)
